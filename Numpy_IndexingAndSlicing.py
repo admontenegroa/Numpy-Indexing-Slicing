94f8381bf1efe013d5891a78723cdadb9a13ed61
Andrés D. Montenegro A <admontenegroa@gmail.com>
2020-11-15 17:48:50 -0500

    Function that exchange the position of two 3x3x3 blocks that make up a 9x9x9 cube

---
+++ Numpy_IndexingAndSlicing.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+def exchange(first_block=0, second_block=17):
+    """Exchange the position of two 3x3x3 blocks that make up
+    a 9x9x9 block.
+
+    The 3x3x3 blocks position is from 0 to 26.
+
+    Keyword arguments:
+    first_block -- the position of the first 3x3x3 block (default 0)
+    second_block -- the position of the second 3x3x3 block (default 17)
+
+    Returns:
+    A 9x9x9 cube where the position of two 3x3x3 cubes was swapped.
+
+    """
+    if first_block > 26 or second_block > 26:
+        raise ValueError("The position exceed the imaginary 3X3X3 block. The position must not be greater than 26!")
+
+    if first_block == second_block:
+        raise ValueError("The position of the two imaginary 3X3X3 block must not be the same!")
+
+    m = np.arange(729).reshape(9, 9, 9)
+
+    block_a = {
+        'z': int(np.floor(first_block / 9)),
+        'y': int(np.floor(first_block % 9 / 3)),
+        'x': first_block % 3
+    }
+
+    A = m[block_a['z'] * 3:block_a['z'] * 3 + 3, block_a['y'] * 3:block_a['y'] * 3 + 3,
+        block_a['x'] * 3:block_a['x'] * 3 + 3]
+
+    block_b = {
+        'z': int(np.floor(second_block / 9)),
+        'y': int(np.floor(second_block % 9 / 3)),
+        'x': second_block % 3
+    }
+
+    B = m[block_b['z'] * 3:block_b['z'] * 3 + 3, block_b['y'] * 3:block_b['y'] * 3 + 3,
+        block_b['x'] * 3:block_b['x'] * 3 + 3]
+
+    print('First Block')
+    print(A)
+    print('\n')
+    print('Second Block')
+    print(B)
+
+    aux = A.copy()
+
+    m[block_a['z'] * 3:block_a['z'] * 3 + 3, block_a['y'] * 3:block_a['y'] * 3 + 3,
+    block_a['x'] * 3:block_a['x'] * 3 + 3] = B
+
+    m[block_b['z'] * 3:block_b['z'] * 3 + 3, block_b['y'] * 3:block_b['y'] * 3 + 3,
+    block_b['x'] * 3:block_b['x'] * 3 + 3] = aux
+
+    return m
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    m = exchange(2,5)
+    print(m)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
